e1e15dfd3e581969de7a89046ee6b81081c41dd3
syslink <7135202@qq.com>
2022-09-18 13:52:24 +0800

    add discord

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -1,9 +1,15 @@
-import { Box } from '@chakra-ui/react';
-
+import { Box, Link, Text } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Footer = () => {
   return (
     <Box textAlign={'center'} w="full" p={6}>      
+      <Text>
+        🙋 You have questions? Ask in the{' '}
+        <Link href={"https://discord.gg/VQ8EA78w"} isExternal alignItems={'center'}>
+          Discord <ExternalLinkIcon />
+        </Link>
+      </Text>
     </Box>
   );
 };
